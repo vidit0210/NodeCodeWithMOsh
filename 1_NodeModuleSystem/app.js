1a2686c2dd6bb96253fffe6acb4fb0476f35ab93
Vidit Shah <vidit0210@gmail.com>
2018-10-29 12:23:50 +0530

    NMS

---
+++ 1_NodeModuleSystem/app.js
@@ -0,0 +1,6 @@
+let logger =require('./logger');
+
+console.log(logger("Yo"));
+
+console.log(__filename);
+console.log(__dirname);
---
+++ 1_NodeModuleSystem/logger.js
@@ -0,0 +1,8 @@
+let url ="www.mywebsite.com";
+
+function log_message(message) {
+    console.log(message);
+}
+
+//module.exports.elog=log_message;
+module.exports = log_message;
